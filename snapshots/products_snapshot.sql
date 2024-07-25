cfeb3ca3488a945dae3ca6412cdf67d3ec1d1fb8
chaitanyabiradar067@gmail.com <chaitanyabiradar067@gmail.com>
2024-07-25 13:32:09 +0000

    snapshot and incremental models

--- snapshots/products_snapshot.sql
+++ snapshots/products_snapshot.sql
@@ -1,12 +1,19 @@
-
 {% snapshot products_snapshot %}
-{{
-    config(
-    target_schema='raw',
-    unique_key='product_id',
-    strategy='check',
-    check_cols=['product_id'],
-)
-}}
-select * from {{ source('raw','products') }}
+
+  {{
+      config(
+        unique_key="product_id",
+        strategy='timestamp',
+        updated_at='lastmodified_date',
+      )
+  }}
+
+select
+    product_id as product_id,
+    product_name as product_name,
+    unitprice as unit_price,
+    created_date as created_date,
+    lastmodified_date as lastmodified_date
+from raw.products
+
 {% endsnapshot %}
---
+++ snapshots/products_snapshot_check.sql
@@ -0,0 +1,20 @@
+{% snapshot products_snapshot_check %}
+
+  {{
+      config(
+        unique_key="product_id",
+        strategy='check',
+        check_cols='all',
+        invalidate_hard_deletes=True,
+      )
+  }}
+
+select
+    product_id as product_id,
+    product_name as product_name,
+    unitprice as unit_price,
+    created_date as created_date,
+    lastmodified_date as lastmodified_date
+from raw.products
+
+{% endsnapshot %}
