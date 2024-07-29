b911597016e971668aa2dea7638fafb2cb0be3e4
Punith Kumar N <703373603@genpact.com>
2024-07-29 11:15:23 +0000

    committing macro changes

--- macros/get_region.sql
+++ macros/get_region.sql
@@ -4,9 +4,13 @@
     {%- set latam_countries = ('Antigua and Barbuda', 'Uruguay') -%}
     {%- set na_countries = ('United States of America', 'Canada') -%}
     case 
-        when {{ country }} in {{ apac_countries }} then '{{ var('apac') }}'
+        when {{ country }} in {{ apac_countries }} then 'APAC'
+        when {{ country }} in {{ emea_countries }} then 'EMEA'
+        when {{ country }} in {{ latam_countries }} then 'LATAM'
+        when {{ country }} in {{ na_countries }} then 'NA'
+        {#when {{ country }} in {{ apac_countries }} then '{{ var('apac') }}'
         when {{ country }} in {{ emea_countries }} then '{{ var('emea') }}'
         when {{ country }} in {{ latam_countries }} then '{{ var('latam') }}'
-        when {{ country }} in {{ na_countries }} then '{{ var('na') }}'
+        when {{ country }} in {{ na_countries }} then '{{ var('na') }}'#}
     end
 {% endmacro %}
