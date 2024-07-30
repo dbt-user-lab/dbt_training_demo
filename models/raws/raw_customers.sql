6ded95da9745ea78c44cb601cd6c034ba12a4971
Ankur Sahu <ankur.sahu@genpact.com>
2024-07-30 09:10:05 +0530

    Added consumtion files

--- models/raws/raw_customers.sql
+++ models/raws/raw_customers.sql
@@ -1,1 +1,7 @@
-select * from {{ source('raw','customers') }}
+select  customer_id, 
+        name, 
+        country, 
+        region_id, 
+        to_date(created_date) as created_date , 
+        to_date(lastmodified_date) as lastmodified_date 
+from {{ source('raw','customers')}}
--- models/raws/raw_sales.sql
+++ models/raws/raw_sales.sql
@@ -1,1 +1,10 @@
-select * from {{ source('raw','sales') }}
+select 
+    order_id, 
+    product_id, 
+    customer_id, 
+    order_date, 
+    order_amount, 
+    created_date, 
+    quantity, 
+    to_date(lastmodified_date) as lastmodified_date
+from {{ source('raw','sales') }}
