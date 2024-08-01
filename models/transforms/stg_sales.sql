8f9bfa3a6b830d1786e92d59e261c57cec7f5c9d
chaitanyabiradar067@gmail.com <chaitanyabiradar067@gmail.com>
2024-08-01 06:55:34 +0000

    Committing changes

--- models/transforms/stg_sales.sql
+++ models/transforms/stg_sales.sql
@@ -8,28 +8,20 @@
 }}
 
 select
-   sales.order_id as order_id,
-   sales.product_id as product_id,
-   sales.customer_id as customer_id,
-   sales.order_date as order_date,
-   sales.order_amount as order_amount,
-   {{ dollars_to_inr('sales.order_amount') }} as order_amount_in_inr,
-   sales.created_date as created_date,
-   sales.quantity as quantity,
-   dim_products.product_name  as product_name ,
-   dim_products.unit_price as product_unit_price,
-   dim_customers.customer_name as customer_name,
-   dim_customers.country  as country,
-   sales.lastmodified_date 
-from {{ ref('raw_sales') }} sales
-left join {{ ref('dim_products')}} dim_products
- on sales.product_id=dim_products.product_id
-left join {{ ref('dim_customers')}} dim_customers
-  on sales.customer_id = dim_customers.customer_id
+   order_id as order_id,
+   product_id as product_id,
+   customer_id as customer_id,
+   order_date as order_date,
+   order_amount as order_amount,
+   {{ dollars_to_inr('order_amount') }} as order_amount_in_inr,
+   created_date as created_date,
+   quantity as quantity,
+   lastmodified_date 
+from {{ ref('raw_sales') }}
 
 {% if is_incremental() %}
 
-    where sales.lastmodified_date > (select max(lastmodified_date) from {{ this }})
+    where lastmodified_date > (select max(lastmodified_date) from {{ this }})
 
 {% endif %}
 
